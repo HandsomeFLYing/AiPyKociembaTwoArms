a274bb919dd1014f5fe54ddbedb3ee8756d8cbb1
FIY <87181516+HandsomeFLYing@users.noreply.github.com>
2025-05-15 23:32:40 +0800

    ser.py

--- RuCuRo-VSCode/ser.py
+++ RuCuRo-VSCode/ser.py
@@ -1,33 +1,8 @@
 import serial
 import time
 
-#def main():
-#    while True:
-#        # 获得接收缓冲区字符
-#        count = ser.inWaiting()
-#        if count != 0:
-#            # 读取内容并显示
-#            recv = ser.read(count)
-#            print(recv)
-#        # 清空接收缓冲区
-#        ser.flushInput()
-#        # 必要的软件延时
-#        time.sleep(0.1)
-#
-#if __name__ == '__main__':
-#    try:
-#    # 打开串口
-#        ser = serial.Serial('/dev/ttyAMA0', 115200)
-#        if ser.isOpen == False:
-#            ser.open()                # 打开串口
-#        ser.write(b"rRZzXfxFRRXX")
-#        #ser.write(b"rzrzRRzRzFZZrZZxfxFXXrZRzRzrZFFzRZRRFFxfxFFXXZZRRZrzRR")
-#        main()
-#    except KeyboardInterrupt:
-#        if ser != None:
-#            ser.close()
 
-    #创建端口对象
+#创建端口对象
 try:
     #传入参数
     arduino_ser = serial.Serial('COM5', 115200)
